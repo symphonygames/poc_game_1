9f63327e852a20a1295a403bb4757253e660e1a6
Davi Souza <davi.souza@upperds.com>
2022-07-13 19:22:27 -0300

    first

---
+++ Assets/player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float Speed;
+    public float JumpForce;
+    public float moveSpeed = 20;
+
+    private Rigidbody2D rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+            
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.right * -moveSpeed * Time.deltaTime;
+            
+        }
+
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.up * -moveSpeed * Time.deltaTime;
+        }
+    }
+}
